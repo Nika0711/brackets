a547aea670ea35b49c57c4856ddaf37030387448
Veronika Pakeleva <veronika.pakeleva@gmail.com>
2022-08-13 00:53:28 +0300

    code vers 1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stackFrom = [];
+  const bracketForPair = {};
+  const openBracket = [];
+
+  for (let j = 0; j < bracketsConfig.length; j++) {
+    bracketForPair[bracketsConfig[j][1]] = bracketsConfig[j][0]
+    openBracket.push(bracketsConfig[j][0])
+    // console.log(openBracket)
+  }
+
+
+  for (let i = 0; i < str.length; i++) {
+    let currentElement = str[i];
+    if (openBracket.includes(currentElement)) {
+      stackFrom.push(currentElement)
+      if ((stackFrom.includes('|') && currentElement === "|")) {
+        stackFrom.pop();
+      }
+    } else {
+      if (stackFrom.length === 0) {
+        return false;
+      }
+      let topElementFromStack = stackFrom[stackFrom.length - 1]
+      if (bracketForPair[currentElement] === topElementFromStack) {
+        stackFrom.pop();
+      } else {
+        return false
+      }
+    }
+
+  } return stackFrom.length === 0;
 }
