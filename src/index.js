933d7e13c9c6786966af3f2d1e924e7a63e1644b
Veronika Pakeleva <94725013+Nika0711@users.noreply.github.com>
2023-02-25 14:28:00 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,33 +1,45 @@
-module.exports = function check(str, bracketsConfig) {
-  const stack = [];
-  const bracketForPair = {};
-  const openBracket = [];
+function check(str, bracketsConfig) {
+  let statusStream = [];
+  let lastStatus = {};
+  let bracketPair = [];
+  let openingBracket = '';
+  let closingBracket = '';
+  let bracketType = 0;
 
-  for (let j = 0; j < bracketsConfig.length; j++) {
-    bracketForPair[bracketsConfig[j][1]] = bracketsConfig[j][0]
-    openBracket.push(bracketsConfig[j][0])
-    // console.log(openBracket)
-  }
-
-
-  for (let i = 0; i < str.length; i++) {
-    let currentElement = str[i];
-    if (openBracket.includes(currentElement)) {
-      stack.push(currentElement)
-      if ((stack.includes('|') && currentElement === "|")) {
-        stack.pop();
+  for (let char of str) {
+    //console.log(`Char: ${char}`);
+    
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      bracketPair = bracketsConfig[i];
+      openingBracket = bracketPair[0];
+      closingBracket = bracketPair[1];
+      bracketType = i + 1;
+      
+      //console.log(`${openingBracket}${closingBracket}`);
+      
+      if (char == closingBracket) {
+        lastStatus = statusStream[statusStream.length - 1];
+        
+        if (lastStatus == bracketType) {
+          statusStream.pop();
+          //console.log(statusStream.toString());
+          break;
+        } else {
+          if (openingBracket != closingBracket) {
+            return false;
+          }
+        }
       }
-    } else {
-      if (stack.length === 0) {
-        return false;
-      }
-      let topElement = stack[stack.length - 1]
-      if (bracketForPair[currentElement] === topElement) {
-        stack.pop();
-      } else {
-        return str.length === 0;
+
+      if (char == openingBracket) {
+        statusStream.push(bracketType);
+        //console.log(statusStream.toString());
+        break;
       }
     }
+  }
 
-  } return stack.length === 0;
+  return (statusStream.length == 0);
 }
+
+module.exports = check;
